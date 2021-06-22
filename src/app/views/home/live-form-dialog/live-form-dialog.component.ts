da5aecf19718caac57b832bfaa9b68e352a5b53b
toshiows <toshio_ws@hotmail.com>
2021-06-22 17:44:52 -0300

    Post live

--- src/app/views/home/live-form-dialog/live-form-dialog.component.ts
+++ src/app/views/home/live-form-dialog/live-form-dialog.component.ts
@@ -1,3 +1,4 @@
+import { LiveService } from './../../../shared/service/live.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
@@ -11,21 +12,32 @@ export class LiveFormDialogComponent implements OnInit {
 
   public liveForm: FormGroup;
 
-  constructor(public dialogRef: MatDialogRef<LiveFormDialogComponent>,
-              private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, 
+    public dialogRef: MatDialogRef<LiveFormDialogComponent>,
+    public rest: LiveService) {
+    
+   }
 
   ngOnInit(): void {
     this.liveForm = this.fb.group({
       liveName: ['', [Validators.required]],
       channelName: ['', [Validators.required]],
       liveLink: ['', [Validators.required]],
-      liveDate: ['', [Validators.required]],
+      liveDate: ['2020-08-01T20:00:00', [Validators.required]],
       liveTime: ['', [Validators.required]]
     });
   }
 
+
+  createLive() {
+    this.rest.postLives(this.liveForm.value).subscribe(result => { });
+
+    this.cancel();
+  }
+
   cancel(): void {
     this.dialogRef.close();
+    this.liveForm.reset();
   }
 
 }
